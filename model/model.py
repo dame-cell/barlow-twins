e3b37561edcb5d1dbb65e9b3b252462c548124fd
cell-dame <122996026+dame-cell@users.noreply.github.com>
2024-09-28 16:50:58 +0530

    Update model.py

--- model/model.py
+++ model/model.py
@@ -39,7 +39,7 @@ class BarlowTwins(nn.Module):
     def __init__(self, config):
         super(BarlowTwins, self).__init__()
         self.config = config
-        self.encoder = torchvision.models.resnet50(zero_init_residual=True)
+        self.encoder = timm.create_model(self.config.model_name, pretrained=self.config.pretrained, num_classes=self.config.num_classes)
         self.projector = MLP(self.config)
         
         # normalization layer for the representations z1 and z2
